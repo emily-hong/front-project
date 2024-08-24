645d35a78bb5e7c2ccd6d04c5bee5d39695bdd23
홍현정 <emilyhong4659@gmail.com>
2024-08-24 21:09:55 +0900

    서브페이지 WIP

---
+++ data.js
@@ -0,0 +1,132 @@
+// data.js
+export const previewData = [
+  {
+    name: "James",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2024/03/07/16/44/stork-8618904_1280.jpg",
+    review:
+      "정말 멋진 경험이었어요! 트리하우스에 있는 모든 순간이 너무 좋았어요. 너무 평화롭고 직원들도 너무 친절했어요!",
+  },
+  {
+    name: "Rachel",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2021/11/23/09/12/mountains-6818253_1280.jpg",
+    review:
+      "반려견과 함께 방문할 때 머무를 수 있는 훌륭한 숙소입니다. 객실은 멋지게 꾸며져 있으며 편안한 숙박을 위해 필요한 시설을 갖추고 있습니다. 다시 돌아올 거예요.",
+  },
+  {
+    name: "Gemma",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2022/10/17/15/02/photography-7527978_1280.jpg",
+    review:
+      "훌륭한 숙소, 멋진 주변 지역, 친절한 호스트가 접근하기 쉽고 따뜻하게 맞아주었습니다.",
+  },
+  {
+    name: "Emily",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2023/05/13/19/36/woman-7991234_1280.jpg",
+    review:
+      "숙박해 주셔서 감사합니다 Megan & Josh! 멋진 위치에 있는 사랑스럽고 편안한 객실. 비켜서서 차가 있어야 돌아다닐 수 있어요. 하지만 놀라운 전망과 시골로 둘러싸인 아름답고 평화로운 위치입니다. ",
+  },
+];
+
+export const allData = [
+  {
+    name: "James",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2024/03/07/16/44/stork-8618904_1280.jpg",
+    review:
+      "정말 멋진 경험이었어요! 트리하우스에 있는 모든 순간이 너무 좋았어요. 너무 평화롭고 직원들도 너무 친절했어요!",
+  },
+  {
+    name: "Rachel",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2021/11/23/09/12/mountains-6818253_1280.jpg",
+    review:
+      "반려견과 함께 방문할 때 머무를 수 있는 훌륭한 숙소입니다. 객실은 멋지게 꾸며져 있으며 편안한 숙박을 위해 필요한 시설을 갖추고 있습니다. 다시 돌아올 거예요.",
+  },
+  {
+    name: "Gemma",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2022/10/17/15/02/photography-7527978_1280.jpg",
+    review:
+      "훌륭한 숙소, 멋진 주변 지역, 친절한 호스트가 접근하기 쉽고 따뜻하게 맞아주었습니다.",
+  },
+  {
+    name: "Emily",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2023/05/13/19/36/woman-7991234_1280.jpg",
+    review:
+      "숙박해 주셔서 감사합니다 Megan & Josh! 멋진 위치에 있는 사랑스럽고 편안한 객실. 비켜서서 차가 있어야 돌아다닐 수 있어요. 하지만 놀라운 전망과 시골로 둘러싸인 아름답고 평화로운 위치입니다. ",
+  },
+  {
+    name: "Tom",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2023/03/31/16/26/maternity-7890356_1280.jpg",
+    review:
+      "아름다운 주변 환경에서의 아늑한 숙박, 주말 동안 꼭 필요했으면 합니다! 날씨가 최고는 아니어서 숙소에 접근하는 것이 좀 더 어려웠어요. 응답이 매우 빠른 호스트이며 현지 팁에 도움이 됩니다.",
+  },
+  {
+    name: "Chacha",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2022/11/08/06/26/woman-7577808_1280.jpg",
+    review:
+      "I had a fantastic time in the swamp! I couldn't have hoped for a better experience, but we were very considerate and took good care of how well it matched Shrek's swamp in the movie. Thank you!",
+  },
+  {
+    name: "Sara",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2024/06/21/07/46/yoga-8843808_640.jpg",
+    review:
+      "이 멋진 숙소에 머무르는 것이 정말 좋았습니다. 설명과 똑같았습니다. 주변을 안내해준 착한 남자의 환영을 받았습니다. 숙소는 매우 깨끗하고 평화롭고 프라이빗했습니다. 자쿠지가 최고였어요! 분위기는 아름다웠습니다. 남편과 저는 첫 결혼 기념일을 축하하고 있었는데, 들어갔을 때 마음으로 모양의 빨간 꽃잎이 아름답게 전시되어 있었습니다! 이 작은 디테일에 대해 매우 감사했습니다.",
+  },
+  {
+    name: "James",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2024/03/07/16/44/stork-8618904_1280.jpg",
+    review:
+      "정말 멋진 경험이었어요! 트리하우스에 있는 모든 순간이 너무 좋았어요. 너무 평화롭고 직원들도 너무 친절했어요!",
+  },
+  {
+    name: "Rachel",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2021/11/23/09/12/mountains-6818253_1280.jpg",
+    review:
+      "반려견과 함께 방문할 때 머무를 수 있는 훌륭한 숙소입니다. 객실은 멋지게 꾸며져 있으며 편안한 숙박을 위해 필요한 시설을 갖추고 있습니다. 다시 돌아올 거예요.",
+  },
+  {
+    name: "Gemma",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2022/10/17/15/02/photography-7527978_1280.jpg",
+    review:
+      "훌륭한 숙소, 멋진 주변 지역, 친절한 호스트가 접근하기 쉽고 따뜻하게 맞아주었습니다.",
+  },
+  {
+    name: "Emily",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2023/05/13/19/36/woman-7991234_1280.jpg",
+    review:
+      "숙박해 주셔서 감사합니다 Megan & Josh! 멋진 위치에 있는 사랑스럽고 편안한 객실. 비켜서서 차가 있어야 돌아다닐 수 있어요. 하지만 놀라운 전망과 시골로 둘러싸인 아름답고 평화로운 위치입니다. ",
+  },
+  {
+    name: "Tom",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2023/03/31/16/26/maternity-7890356_1280.jpg",
+    review:
+      "아름다운 주변 환경에서의 아늑한 숙박, 주말 동안 꼭 필요했으면 합니다! 날씨가 최고는 아니어서 숙소에 접근하는 것이 좀 더 어려웠어요. 응답이 매우 빠른 호스트이며 현지 팁에 도움이 됩니다.",
+  },
+  {
+    name: "Chacha",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2022/11/08/06/26/woman-7577808_1280.jpg",
+    review:
+      "I had a fantastic time in the swamp! I couldn't have hoped for a better experience, but we were very considerate and took good care of how well it matched Shrek's swamp in the movie. Thank you!",
+  },
+  {
+    name: "Sara",
+    profileUrl:
+      "https://cdn.pixabay.com/photo/2024/06/21/07/46/yoga-8843808_640.jpg",
+    review:
+      "이 멋진 숙소에 머무르는 것이 정말 좋았습니다. 설명과 똑같았습니다. 주변을 안내해준 착한 남자의 환영을 받았습니다. 숙소는 매우 깨끗하고 평화롭고 프라이빗했습니다. 자쿠지가 최고였어요! 분위기는 아름다웠습니다. 남편과 저는 첫 결혼 기념일을 축하하고 있었는데, 들어갔을 때 마음으로 모양의 빨간 꽃잎이 아름답게 전시되어 있었습니다! 이 작은 디테일에 대해 매우 감사했습니다.",
+  },
+];
